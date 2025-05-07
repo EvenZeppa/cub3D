bced6c7ee20e3cc5beb3f21571da967dd39e822e
Even Zeppa <zeppaeven.dev@gmail.com>
2025-05-07 15:40:38 +0200

    UPD main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include "cub3d.h"
 
+int launch_game(t_app *app)
+{
+
+    return (0);
+}
+
 int main(int argc, char *argv[])
 {
     t_app app;
+
     if (argc != 2)
         return (printf("Usage : ./cub3D map.cub"));
-    init_game(&app, argv[1]);
-    launch_game(&app);
-    free_game(&app);
+    if (init_game(&app, argv[1]))
+        exit_error("Init game");
+    if (launch_game(&app))
+        exit_error("Launch game");
+    if (free_game(&app))
+        exit_error("Free game");
     return (0);
 }
